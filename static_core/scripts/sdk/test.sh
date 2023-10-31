0ea9cf4ee53f295758323c3ae0ac9ff56ab67b5f
Georgy Bronnikov <bronnikov.georgy@huawei.com>
2023-10-31 21:56:00 +0300

    Support loadable plugins in es2panda
    Issue: #I8I1XN
    
    Description:
      - prefixes/suffixes for shared libraries
      - new items in sdk file lists
    
    Signed-off-by: Georgy Bronnikov <bronnikov.georgy@huawei.com>

--- static_core/scripts/sdk/test.sh
+++ static_core/scripts/sdk/test.sh
@@ -106,6 +106,7 @@ build-sdk/sdk/ohos_arm64/include/platforms
 build-sdk/sdk/ohos_arm64/include/plugins
 build-sdk/sdk/ohos_arm64/include/runtime
 build-sdk/sdk/ohos_arm64/include/third_party
+build-sdk/sdk/ohos_arm64/include/tools
 build-sdk/sdk/ohos_arm64/include/verification
 build-sdk/sdk/ohos_arm64/lib
 build-sdk/sdk/ohos_arm64/lib/libarkaotmanager.so
@@ -118,6 +119,7 @@ build-sdk/sdk/ohos_arm64/lib/libarkruntime_static.a
 build-sdk/sdk/ohos_arm64/lib/libarkziparchive.so
 build-sdk/sdk/ohos_arm64/lib/libc_secshared.so
 build-sdk/sdk/ohos_arm64/lib/libdprof.a
+build-sdk/sdk/ohos_arm64/lib/libe2p_test_plugin.so
 build-sdk/sdk/ohos_arm64/lib/libes2panda-lib.so
 build-sdk/sdk/ohos_arm64/lib/libets_interop_js_napi.so
 build-sdk/sdk/ohos_arm64/lib/libhmicui18n.z.so
