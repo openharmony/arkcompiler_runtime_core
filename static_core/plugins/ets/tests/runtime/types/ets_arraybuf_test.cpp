d78e351c3e14b94079b9fb9c6d41f988a2cf7f28
cheezzario <windy.road123@huawei-partners.com>
2024-03-07 18:09:32 +0300

    Title: Implement ArrayBuffer class Description: TypeScript API compatible ArrayBuffer implementation Issue: https://gitee.com/openharmony/arkcompiler_runtime_core/issues/I97LLU Testing: ets_func_tests (escompat/ArrayBufferTest1 & escompat/ArrayBufferTest2)
    Signed-off-by: cheezzario <windy.road123@huawei-partners.com>

--- static_core/plugins/ets/tests/runtime/types/ets_arraybuf_test.cpp
+++ static_core/plugins/ets/tests/runtime/types/ets_arraybuf_test.cpp
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2023-2024 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -56,7 +56,7 @@ public:
 
     static std::vector<MirrorFieldInfo> GetMembers()
     {
-        return std::vector<MirrorFieldInfo> {MIRROR_FIELD_INFO(EtsArrayBuffer, byteLength_, "byteLength"),
+        return std::vector<MirrorFieldInfo> {MIRROR_FIELD_INFO(EtsArrayBuffer, byteLength_, "_byteLength"),
                                              MIRROR_FIELD_INFO(EtsArrayBuffer, data_, "data")};
     }
 
