cf180c1c18b0eb6aec3b22a7edb381122f07bc21
hufeng <hufeng20@huawei.com>
2024-03-21 14:06:34 +0000

    Adjust verifier
    Signed-off-by: hufeng <hufeng20@huawei.com>
    Change-Id: I3531f146da4549776683045e3922c251c24fe25b

--- verifier/verifier.cpp
+++ verifier/verifier.cpp
@@ -235,13 +235,14 @@ bool Verifier::CollectIdInInstructions(const panda_file::File::EntityId &method_
     return true;
 }
 
-void Verifier::CollectModuleLiteralId(const panda_file::File::EntityId &field_id)
+void Verifier::CollectModuleLiteralId(panda_file::FieldDataAccessor &field_accessor)
 {
-    panda_file::FieldDataAccessor field_accessor(*file_, field_id);
-    const auto literal_id = field_accessor.GetValue<uint32_t>().value();
-    if (std::find(literal_ids_.begin(), literal_ids_.end(), literal_id) != literal_ids_.end()) {
-        module_literals_.insert(literal_id);
+    panda_file::StringData sd = file_->GetStringData(field_accessor.GetNameId());
+    if (std::strcmp(utf::Mutf8AsCString(sd.data), MODULE_IDX_FIELD_NAME) != 0) {
+        return;
     }
+    auto module_literal_offset = field_accessor.GetValue<int32_t>().value();
+    module_literals_.insert(module_literal_offset);
 }
 
 bool Verifier::CheckConstantPool(const verifier::ActionType type)
@@ -266,7 +267,7 @@ bool Verifier::CheckConstantPool(const verifier::ActionType type)
             }
             if (type == verifier::ActionType::COLLECTINFOS) {
                 class_accessor.EnumerateFields([&](panda_file::FieldDataAccessor &field_accessor) -> void {
-                    CollectModuleLiteralId(field_accessor.GetFieldId());
+                    CollectModuleLiteralId(field_accessor);
                 });
             }
         }
