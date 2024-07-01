5148e80aa5ea311539372f1a8e5246b6d0c28234
Malinin Andrey <malinin.andrey@huawei-partners.com>
2024-07-01 17:07:59 +0800

    Initial BlockingQueue implementation
    Issue: #IAAECL
    Tests: ninja all tests
    Signed-off-by: Malinin Andrey <malinin.andrey@huawei-partners.com>

--- static_core/plugins/ets/runtime/types/ets_primitives.h
+++ static_core/plugins/ets/runtime/types/ets_primitives.h
@@ -12,8 +12,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#ifndef PANDA_RUNTIME_ETS_TYPES_ETS_PRIMITIVES_H_
-#define PANDA_RUNTIME_ETS_TYPES_ETS_PRIMITIVES_H_
+#ifndef PANDA_RUNTIME_ETS_TYPES_ETS_PRIMITIVES_H
+#define PANDA_RUNTIME_ETS_TYPES_ETS_PRIMITIVES_H
 
 #include <cstdint>
 #include <type_traits>
@@ -39,4 +39,4 @@ constexpr EtsBoolean ToEtsBoolean(bool b)
 
 }  // namespace ark::ets
 
-#endif  // PANDA_RUNTIME_ETS_TYPES_ETS_PRIMITIVES_H_
+#endif  // PANDA_RUNTIME_ETS_TYPES_ETS_PRIMITIVES_H
