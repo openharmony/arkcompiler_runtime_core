271e6f2e1af803119fe3fe291c4cc7ace987aaf3
Daniil Kofanov <kofanov.daniil@huawei-partners.com>
2024-09-30 23:00:02 +0800

    [Interop] Refactor interop benchmarks
    Add infrastructure to run JS->JS, JS->STS, STS->JS, STS->STS benchmarks.
    
    Issue: https://gitee.com/openharmony/arkcompiler_runtime_core/issues/IAUT7C
    Signed-off-by: Daniil Kofanov <kofanov.daniil@huawei-partners.com>

--- static_core/plugins/ets/tests/interop_js/tests/perf/benchmarks/callee.js
+++ static_core/plugins/ets/tests/interop_js/tests/perf/benchmarks/callee.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2021-2024 Huawei Device Co., Ltd.
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -14,15 +14,18 @@
  */
 'use strict';
 
-const o = { a: 1, b: '2', c: {} };
+let o = { a: 1, b: '2', c: {} };
 
-exports.GetObject = function () {
+module.exports.GetObject = function GetObject() {
     return o;
 };
-exports.GetNumber = function () {
+module.exports.GetNumber = function GetNumber() {
     return 24;
 };
-exports.GetString = function () {
+module.exports.GetString = function GetString() {
     return 'abc';
 };
-exports.Nop = function () {};
+module.exports.GetArray = function GetArray() {
+    return [1, 2, 5];
+};
+module.exports.Pass = function Pass() {};
