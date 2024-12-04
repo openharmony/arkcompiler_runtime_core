88d8dafb6b31e24bdfd400566c8b8643085456e9
Khomutov Nikita <khomutov.nikita@huawei.com>
2024-12-04 17:29:32 +0300

    [abckit] source lang support (khomutov nikita)
    Issue: https://gitee.com/openharmony/arkcompiler_runtime_core/issues/IB9FIY
    
    Change-Id: Ifbf379d58002fe3974b3b6fb056358224962e8af
    Signed-off-by: Nazarov Konstantin <nazarov.konstantin1@huawei.com>

--- libabckit/src/adapter_dynamic/helpers_dynamic.cpp
+++ libabckit/src/adapter_dynamic/helpers_dynamic.cpp
@@ -19,6 +19,7 @@
 #include "libabckit/src/logger.h"
 #include "libabckit/src/statuses_impl.h"
 #include "assembler/assembly-program.h"
+#include "assembler/assembly-record.h"
 
 #if defined(_WIN32)
 #include <cwchar>
@@ -255,7 +256,7 @@ const panda_file::File *EmitDynamicProgram(AbckitFile *file, pandasm::Program *p
                                            std::string_view path)
 {
     for (auto &[recName, rec] : program->record_table) {
-        if (IsServiceRecord(recName) || IsAnnotationInterfaceRecord(rec) || IsExternalRecord(rec)) {
+        if (!IsModuleDescriptorRecord(rec) || IsExternalRecord(rec)) {
             continue;
         }
         if (!UpdateModuleLiteralArray(file, recName)) {
@@ -472,6 +473,24 @@ bool IsServiceRecord(const std::string &name)
     return name == TSTYPE_ANNO_RECORD_NAME || name == "_ESConcurrentModuleRequestsAnnotation" ||
            name == "L_ESSlotNumberAnnotation" || name == "_ESSlotNumberAnnotation" ||
            name == "_ESExpectedPropertyCountAnnotation";
+}
+
+bool IsModuleDescriptorRecord(const pandasm::Record &rec)
+{
+    constexpr std::array<std::string_view, 2> REQUIRED_FIELDS = {
+        "moduleRecordIdx",
+        "scopeNames",
+    };
+
+    for (const auto &fieldName : REQUIRED_FIELDS) {
+        auto it = std::find_if(rec.field_list.begin(), rec.field_list.end(),
+                               [&fieldName](const auto &field) { return fieldName == field.name; });
+        if (it == rec.field_list.end()) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 bool IsAnnotationInterfaceRecord(const pandasm::Record &rec)
