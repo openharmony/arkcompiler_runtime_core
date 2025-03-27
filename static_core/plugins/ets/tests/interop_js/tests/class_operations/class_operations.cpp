b53c2e2184dcac86183044992ac4fa5fde4e62fa
groshevmaksim <groshev.maksim@h-partners.com>
2025-03-27 18:31:37 +0300

    Test migration with hasDecl
    Tests: Interop tests
    Issue: https://gitee.com/openharmony/arkcompiler_runtime_core/issues/IBYNVA
    
    Signed-off-by: groshevmaksim <groshev.maksim@h-partners.com>
    Change-Id: I7da7cf6b57558623e023209bb773da672db14bda

--- static_core/plugins/ets/tests/interop_js/tests/class_operations/class_operations.cpp
+++ static_core/plugins/ets/tests/interop_js/tests/class_operations/class_operations.cpp
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2021-2024 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -22,12 +22,8 @@ class EtsInteropClassOperationsTest : public EtsInteropTest {
 public:
     void SetUp() override
     {
-        interopJsTestPath_ = std::getenv("ARK_ETS_INTEROP_JS_GTEST_SOURCES");
-        // This object is used to save global js names
-        if (!SetGtestEnv()) {
-            std::abort();
-        }
-        LoadModuleAs("module", "module.js");
+        EtsInteropTest::SetUp();
+        LoadModuleAs("module", "module");
     }
 };
 
--- static_core/plugins/ets/tests/interop_js/tests/scenarios/ets_to_js/js_suites/test_boxed_Boolean_conversion.js
+++ static_core/plugins/ets/tests/interop_js/tests/scenarios/ets_to_js/js_suites/test_boxed_Boolean_conversion.js
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-const { etsVm, getTestModule } = require('scenarios.test.js');
+const { etsVm, getTestModule } = require('../scenarios.test.abc');
 
 const etsMod = getTestModule('scenarios_test');
 const GCJSRuntimeCleanup = etsMod.getFunction('GCJSRuntimeCleanup');
