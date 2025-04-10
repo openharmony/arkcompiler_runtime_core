25fef84cc4d92f88dada70379ed5129075513a9b
molotkovmikhail <molotkov.mikhail@huawei-partners.com>
2025-04-10 23:32:33 +0800

    Resolving except of rejection in Promise.then
    Fix bug with rejection in then
    
    Issue: https://gitee.com/openharmony/arkcompiler_runtime_core/issues/IC073S
    
    Testing: `ninja all tests`
    
    Signed-off-by: molotkovmikhail <molotkov.mikhail@huawei-partners.com>

--- static_core/plugins/ets/tests/interop_js/tests/promise/run_promise_test.js
+++ static_core/plugins/ets/tests/interop_js/tests/promise/run_promise_test.js
@@ -25,6 +25,7 @@ function runTest(test) {
 		'panda-files': gtestAbcPath,
 		'boot-panda-files': `${stdlibPath}:${gtestAbcPath}`,
 		'coroutine-enable-external-scheduling': 'true',
+		'xgc-trigger-type': 'never',
 	};
 	if (!etsVm.createRuntime(etsOpts)) {
 		throw Error('Cannot create ETS runtime');
