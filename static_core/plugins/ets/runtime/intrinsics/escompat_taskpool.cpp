6de6d78141b5a05a80d57474ffcb8531379edec5
zhuangkudecha <fuhao71@huawei.com>
2025-05-07 16:09:43 +0800

    Add option for taskpool
    Issue: https://gitee.com/openharmony/arkcompiler_runtime_core/issues/IC618C
    
    Description: Add option for taskpool using launch or EAWorker
    Tests: ninja all tests
    
    Signed-off-by: zhuangkudecha <fuhao71@huawei.com>

--- static_core/plugins/ets/runtime/intrinsics/escompat_taskpool.cpp
+++ static_core/plugins/ets/runtime/intrinsics/escompat_taskpool.cpp
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Copyright (c) 2024-2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -16,12 +16,14 @@
 #include <atomic>
 #include "libpandabase/os/time.h"
 #include "plugins/ets/runtime/types/ets_primitives.h"
+#include "runtime/include/runtime.h"
 
 namespace ark::ets::intrinsics::taskpool {
 
 static std::atomic<EtsLong> g_taskId = 1;
 static std::atomic<EtsLong> g_taskGroupId = 1;
 static std::atomic<EtsLong> g_seqRunnerId = 1;
+static constexpr const char *EA_WORKER = "eaworker";
 
 extern "C" EtsLong GenerateTaskId()
 {
@@ -38,4 +40,12 @@ extern "C" EtsLong GenerateSeqRunnerId()
     return g_seqRunnerId++;
 }
 
+extern "C" EtsBoolean IsUsingLaunchMode()
+{
+    const auto &taskpoolMode =
+        Runtime::GetOptions().GetTaskpoolMode(plugins::LangToRuntimeType(panda_file::SourceLang::ETS));
+
+    bool res = (taskpoolMode == EA_WORKER);
+    return ark::ets::ToEtsBoolean(res);
+}
 }  // namespace ark::ets::intrinsics::taskpool
