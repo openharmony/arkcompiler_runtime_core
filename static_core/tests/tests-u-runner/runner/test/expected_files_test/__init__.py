43d8c24d118ee51f0f108269f26808606844f9d5
Elizaveta Kozhevnikova <kozhevnikova.elizaveta@huawei-partners.com>
2025-06-05 12:35:35 +0300

    URunner: compare expected file with std.ERR
    Issue: https://gitee.com/openharmony/arkcompiler_runtime_core/issues/ICCXHE
    
    Testing: All required pre-merge tests passed. Results are available in the ggwatcher.
    
    Signed-off-by: Elizaveta Kozhevnikova <kozhevnikova.elizaveta@huawei-partners.com>

--- static_core/tests/tests-u-runner/runner/test/expected_files_test/__init__.py
+++ static_core/tests/tests-u-runner/runner/test/expected_files_test/__init__.py
@@ -1,4 +1,7 @@
-# Copyright (c) 2025 Huawei Device Co., Ltd.
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2021-2025 Huawei Device Co., Ltd.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -10,10 +13,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
----
-cases:
-  - use: |-
-      let a = [3, 4, 5]
-      let expectedException = (e: Error | Exception): boolean => {return (e instanceof RangeError)}
-      arktest.expectThrow(() => {a[3] = 7}, expectedException) // Runtime Error RangeError: idx = 3; length = 3
