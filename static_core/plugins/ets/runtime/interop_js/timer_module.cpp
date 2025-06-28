ae8a6c04d34e6cead73f772dd5836dda868622dc
yangbinfa <yangbinfa@huawei.com>
2025-06-28 10:21:06 +0800

    [BugFix] clearTimer double free
    Issue: https://gitee.com/openharmony/arkcompiler_runtime_core/issues/ICIFDI
    
    Signed-off-by: yangbinfa <yangbinfa@huawei.com>

--- static_core/plugins/ets/runtime/interop_js/timer_module.cpp
+++ static_core/plugins/ets/runtime/interop_js/timer_module.cpp
@@ -154,7 +154,6 @@ void TimerModule::TimerCallback(uv_timer_t *timer)
         auto timerId = reinterpret_cast<uint64_t>(timerHandle->data);
         auto [info, exists] = FindTimerInfo(env, timerId);
         if (!exists) {
-            DisarmTimer(timerHandle);
             return;
         }
 
