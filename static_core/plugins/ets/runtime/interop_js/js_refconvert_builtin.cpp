6a441336cb9a30aa512ee8f4b8f1c060cdd68d6c
Parshukov Kirill <parshukov.kirill1@huawei-partners.com>
2025-07-29 12:13:56 +0300

    Fix NegativeArraySizeError
    Issue: https://gitee.com/open_harmony/dashboard?issue_id=ICM9NE
    
    Testing: all pre-merge tests passed
    
    Signed-off-by: Parshukov Kirill <parshukov.kirill1@huawei-partners.com>

--- static_core/plugins/ets/runtime/interop_js/js_refconvert_builtin.cpp
+++ static_core/plugins/ets/runtime/interop_js/js_refconvert_builtin.cpp
@@ -155,7 +155,6 @@ private:
             std::make_tuple("Lstd/core/IllegalArgumentError;", NO_MIRROR, &W_ERROR_OVERLOADS),
             std::make_tuple("Lstd/core/IndexOutOfBoundsError;", NO_MIRROR, &W_ERROR_OVERLOADS),
             std::make_tuple("Lstd/core/ArrayStoreError;", NO_MIRROR, &W_ERROR_OVERLOADS),
-            std::make_tuple("Lstd/core/NegativeArraySizeError;", NO_MIRROR, &W_ERROR_OVERLOADS),
             std::make_tuple("Lstd/core/NoDataError;", NO_MIRROR, &W_ERROR_OVERLOADS),
             std::make_tuple("Lstd/core/LinkerError;", NO_MIRROR, &W_ERROR_OVERLOADS),
             std::make_tuple("Lstd/core/LinkerClassNotFoundError;", NO_MIRROR, &W_ERROR_OVERLOADS),
