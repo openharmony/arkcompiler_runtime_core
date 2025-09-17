8260a81d2a4d42e0c6012b9f1eaf0f1961f7ded5
Zelentsov Dmitry <zelentsov.dmitry@huawei-partners.com>
2025-09-17 16:44:05 +0300

    Prohibit implicit export
    Issue: https://gitcode.com/openharmony/arkcompiler_ets_frontend/issues/7589
    
    Tests: use Test-U-Runner and CI
    
    Signed-off-by: Zelentsov Dmitry <zelentsov.dmitry@huawei-partners.com>

--- static_core/runtime/intrinsics.cpp
+++ static_core/runtime/intrinsics.cpp
@@ -443,5 +443,7 @@ void Memsetf64(ObjectHeader *array, double value, uint32_t initialIndex, uint32_
 }  // namespace ark::intrinsics
 
 #include "core_any_intrinsics.inc"
+
 #include "runtime/entrypoints/entrypoints.h"
+
 #include <intrinsics_gen.h>
