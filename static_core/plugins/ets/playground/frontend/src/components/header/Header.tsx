ba84e46def401f6b552a684706d9afead9c093ab
dr-irina <bugorkova.irina@huawei-partners.com>
2025-11-20 04:07:12 +0000

    pg docs link
    Issue: https://gitcode.com/openharmony/arkcompiler_runtime_core/issues/8382
    
    Testing: tests were added
    Signed-off-by: dr-irina <bugorkova.irina@huawei-partners.com>

--- static_core/plugins/ets/playground/frontend/src/components/header/Header.tsx
+++ static_core/plugins/ets/playground/frontend/src/components/header/Header.tsx
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Copyright (c) 2024-2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -141,6 +141,17 @@ const Header = (): JSX.Element => {
             data-testid="header-component"
         >
             <span className={cx({[styles.title]: true})}>ArkTS playground</span>
+            <div className={styles.linksContainer}>
+                <a
+                    href="https://gitcode.com/igelhaus/arkcompiler_runtime_core/releases"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className={styles.link}
+                    data-testid="docs-link"
+                >
+                    Docs
+                </a>
+            </div>
             <Popover
                 content={exampleMenu(
                     theme,
