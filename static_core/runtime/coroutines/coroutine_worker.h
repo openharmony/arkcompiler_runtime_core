4ba0437dec9be9565f0a019e4354b533f93e334a
atlantiswang <wanghao750@h-partners.com>
2026-01-12 16:06:56 +0800

    coroutine queue recycle in time
    Issue:https://gitcode.com/openharmony/arkcompiler_runtime_core/issues/9196
    Signed-off-by: atlantiswang <wanghao750@h-partners.com>

--- static_core/runtime/coroutines/coroutine_worker.h
+++ static_core/runtime/coroutines/coroutine_worker.h
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2024-2025 Huawei Device Co., Ltd.
+ * Copyright (c) 2024-2026 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -113,6 +113,7 @@ public:
     }
 
     void OnCoroBecameActive(Coroutine *co);
+    virtual void OnNewCoroutineStartup([[maybe_unused]] Coroutine *co) {};
 
     void TriggerSchedulerExternally(Coroutine *requester, int64_t delayMs = 0);
 
--- static_core/runtime/coroutines/stackful_coroutine.cpp
+++ static_core/runtime/coroutines/stackful_coroutine.cpp
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2023-2025 Huawei Device Co., Ltd.
+ * Copyright (c) 2023-2026 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -138,6 +138,8 @@ void StackfulCoroutineContext::ThreadProcImpl()
     GetWorker()->GetPerfStats().FinishInterval(CoroutineTimeStats::CTX_SWITCH);
     // consider changing this to INIT later on...
     GetWorker()->GetPerfStats().StartInterval(CoroutineTimeStats::SCH_ALL);
+    // new coroutine startup event
+    GetWorker()->OnNewCoroutineStartup(co);
 
     co->NativeCodeBegin();
     SetStatus(Coroutine::Status::RUNNING);
