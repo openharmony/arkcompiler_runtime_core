c3acc03e6553ae493fbb3d9dbb2bd89a96a9abb7
chenwendi4 <chenwendi4@huawei.com>
2026-01-19 15:02:15 +0800

    Add GC log after app cold start finishes
    Issue: https://gitcode.com/openharmony/arkcompiler_runtime_core/issues/9276
    
    Signed-off-by: chenwendi4 <chenwendi4@huawei.com>

--- static_core/plugins/ets/runtime/interop_js/interop_context.cpp
+++ static_core/plugins/ets/runtime/interop_js/interop_context.cpp
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2023-2025 Huawei Device Co., Ltd.
+ * Copyright (c) 2023-2026 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -106,6 +106,7 @@ static void AppStateCallback(int state, int64_t timeStamp)
             [[fallthrough]];
         case AppState::State::COLD_START_FINISHED:
             etsVm->GetGC()->PostponeGCEnd();
+            LOG(INFO, GC) << "App cold start finished";
             break;
         default:
             break;
