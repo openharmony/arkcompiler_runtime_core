a407dc8f5b2fbc9445382ffe79d71e35babede97
fly971205 <hutongfei@huawei.com>
2026-01-20 19:56:48 +0800

    add tojson operation
    Issue:https://gitcode.com/openharmony/arkcompiler_runtime_core/issues/9313
    
    Signed-off-by: fly971205 <hutongfei@huawei.com>

--- static_core/plugins/ets/runtime/intrinsics/helpers/json_helper.h
+++ static_core/plugins/ets/runtime/intrinsics/helpers/json_helper.h
@@ -1,5 +1,5 @@
-/*
- * Copyright (c) 2025 Huawei Device Co., Ltd.
+/**
+ * Copyright (c) 2025-2026 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -74,6 +74,7 @@ private:
     bool SerializeEmptyObject();
     bool SerializeJSONNullValue();
     bool SerializeJSONRecord(EtsHandle<EtsObject> &value);
+    bool SerializeJsonSerializable(EtsHandle<EtsObject> &value);
 
     bool PushValue(EtsHandle<EtsObject> &value);
     void PopValue(EtsHandle<EtsObject> &value);
