d005e06d33b0201a73bc5e8824bf050f399442cc
dr-irina <bugorkova.irina@huawei-partners.com>
2026-01-27 05:15:05 +0000

    add playwrite tests
    Issue: https://gitcode.com/openharmony/arkc1gitompiler_runtime_core/issues/9469
    
    Testing: tests were added
    Signed-off-by: dr-irina <bugorkova.irina@huawei-partners.com>

--- static_core/plugins/ets/playground/frontend/jest.config.js
+++ static_core/plugins/ets/playground/frontend/jest.config.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Copyright (c) 2024-2026 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -60,6 +60,7 @@ module.exports = {
         '<rootDir>/__tests__/mocks/',
         '<rootDir>/__tests__/constants/',
         '<rootDir>/__tests__/utils/',
+        '<rootDir>/e2e/',
     ],
     automock: false,
 };
