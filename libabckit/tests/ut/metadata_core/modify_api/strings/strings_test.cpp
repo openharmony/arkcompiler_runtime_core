d73a04563b2d4fcfb0e26de27620cad3b3707cea
Zelentsov Dmitry <zelentsov.dmitry@huawei-partners.com>
2026-03-11 12:34:13 +0300

    Refactor apparent type usage in CodeGen
    Issue: https://gitcode.com/openharmony/arkcompiler_ets_frontend/issues/9489
    
    Tests: use Test-U-Runner and CI
    
    Co-Authored-By: NA
    
    Signed-off-by: Zelentsov Dmitry <zelentsov.dmitry@huawei-partners.com>

--- libabckit/tests/ut/metadata_core/modify_api/strings/strings_test.cpp
+++ libabckit/tests/ut/metadata_core/modify_api/strings/strings_test.cpp
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Copyright (c) 2024-2026 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -126,13 +126,12 @@ TEST_F(LibAbcKitModifyApiStringsTest, StaticCreateString2)
                   {2},
                   {
                       {0, ABCKIT_ISA_API_STATIC_OPCODE_LOADSTRING, {}},
-                      {1, ABCKIT_ISA_API_STATIC_OPCODE_CHECKCAST, {0}},
-                      {2, ABCKIT_ISA_API_STATIC_OPCODE_CALL_STATIC, {0}},
-                      {3, ABCKIT_ISA_API_STATIC_OPCODE_LOADSTRING, {}},
-                      {4, ABCKIT_ISA_API_STATIC_OPCODE_CALL_STATIC, {3}},
-                      {5, ABCKIT_ISA_API_STATIC_OPCODE_LOADSTRING, {}},
-                      {6, ABCKIT_ISA_API_STATIC_OPCODE_CALL_STATIC, {5}},
-                      {7, ABCKIT_ISA_API_STATIC_OPCODE_RETURN_VOID, {}},
+                      {1, ABCKIT_ISA_API_STATIC_OPCODE_CALL_STATIC, {0}},
+                      {2, ABCKIT_ISA_API_STATIC_OPCODE_LOADSTRING, {}},
+                      {3, ABCKIT_ISA_API_STATIC_OPCODE_CALL_STATIC, {2}},
+                      {4, ABCKIT_ISA_API_STATIC_OPCODE_LOADSTRING, {}},
+                      {5, ABCKIT_ISA_API_STATIC_OPCODE_CALL_STATIC, {4}},
+                      {6, ABCKIT_ISA_API_STATIC_OPCODE_RETURN_VOID, {}},
                   }},
                  {{1}, {}, {}}});
             helpers::VerifyGraph(graph, bbSchemas);
