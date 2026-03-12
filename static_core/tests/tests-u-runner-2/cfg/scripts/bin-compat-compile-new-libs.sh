53f41cfa49a93e46f7b37dd8ba8d42a4c16c6956
Vladimir Lind <lind.vladimir2@huawei-partners.com>
2026-03-12 18:30:53 +0300

    Description: CTS binary compat tests harness
    Reason: Binary compatibility tests
    
    Co-Authored-By: Agent
    
    Issue: https://gitcode.com/openharmony/arkcompiler_runtime_core/issues/9941
    Signed-off-by: Vladimir Lind <lind.vladimir2@huawei-partners.com>

---
+++ static_core/tests/tests-u-runner-2/cfg/scripts/bin-compat-compile-new-libs.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+# Copyright (c) 2026 Huawei Device Co., Ltd.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Recompile all libs that had .new.ets replacements.
+# Only libs with a .new.ets counterpart are recompiled; others keep their
+# existing .abc from the initial compilation.
+#
+# Usage: bin-compat-compile-new-libs.sh <es2panda> <arktsconfig> <extension>
+#        <opt-level> <gen-prefix> <intermediate-prefix>
+#
+# Arguments:
+#   es2panda             - path to es2panda binary
+#   arktsconfig          - path to arktsconfig.json
+#   extension            - source extension (ets)
+#   opt-level            - optimization level (0-2)
+#   gen-prefix           - ${work-dir}/gen/${test-id-name}
+#   intermediate-prefix  - ${work-dir}/intermediate/${test-id-name}
+
+set -e
+
+ES2PANDA="$1"
+ARKTSCONFIG="$2"
+EXTENSION="$3"
+OPT_LEVEL="$4"
+GEN_PREFIX="$5"
+INTERMEDIATE_PREFIX="$6"
+
+compiled=0
+for new_lib in "${GEN_PREFIX}".libr*.new.ets; do
+    [ -f "$new_lib" ] || continue
+
+    # Source: .libr.new.ets -> .libr.ets (already overwritten by copy-new-libs)
+    lib_file="${new_lib%.new.ets}.ets"
+
+    # Output ABC: swap gen/ prefix for intermediate/, append .abc
+    # gen/test.libr.ets -> intermediate/test.libr.ets.abc
+    output_abc="${INTERMEDIATE_PREFIX}${lib_file#${GEN_PREFIX}}.abc"
+
+    "$ES2PANDA" \
+        "--arktsconfig=${ARKTSCONFIG}" \
+        "--gen-stdlib=false" \
+        "--extension=${EXTENSION}" \
+        "--opt-level=${OPT_LEVEL}" \
+        "--output=${output_abc}" \
+        "$lib_file"
+
+    compiled=$((compiled + 1))
+done
---
+++ static_core/tests/tests-u-runner-2/cfg/scripts/bin-compat-copy-new-libs.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Copyright (c) 2026 Huawei Device Co., Ltd.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Copy <test>.lib<N>.new.ets -> <test>.lib<N>.ets for all new lib versions.
+# Libs without a .new.ets counterpart are left untouched.
+#
+# Usage: bin-compat-copy-new-libs.sh <gen-dir>/<test-id-name>
+# Example: bin-compat-copy-new-libs.sh /path/gen/23.binary_compatibility/test
+
+set -e
+
+TEST_ID_PATH="$1"
+
+copied=0
+for new_lib in "${TEST_ID_PATH}".libr*.new.ets; do
+    [ -f "$new_lib" ] || continue
+    old_lib="${new_lib%.new.ets}.ets"
+    cp "$new_lib" "$old_lib"
+    copied=$((copied + 1))
+done
