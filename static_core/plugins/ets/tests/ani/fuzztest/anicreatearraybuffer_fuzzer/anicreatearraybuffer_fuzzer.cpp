33608cb07a464bdedaef348c5006c46afeb64c87
jichunjiang-30079441 <jichunjiang1@h-partners.com>
2026-03-12 20:13:22 +0800

    Updata Fuzz test case
    Issue: https://gitcode.com/openharmony/arkcompiler_runtime_core/issues/10371
    Co-Authored-By: NA
    Signed-off-by: jichunjiang-30079441 <jichunjiang1@h-partners.com>

--- static_core/plugins/ets/tests/ani/fuzztest/anicreatearraybuffer_fuzzer/anicreatearraybuffer_fuzzer.cpp
+++ static_core/plugins/ets/tests/ani/fuzztest/anicreatearraybuffer_fuzzer/anicreatearraybuffer_fuzzer.cpp
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Copyright (c) 2026 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -18,6 +18,7 @@
 #include "test_common.h"
 #include <cstddef>
 #include <cstdint>
+#include <fuzzer/FuzzedDataProvider.h>
 // NOLINTBEGIN(cppcoreguidelines-pro-type-vararg, modernize-avoid-c-arrays, readability-magic-numbers)
 
 namespace OHOS {
@@ -35,7 +36,8 @@ void AniCreateArrayBufferFuzzTest(const char *data, size_t size)
     engine->GetAniEnv(&env);
     ani_arraybuffer arrayBufferRes {};
     void *dataResult {};
-    auto bufferSize = static_cast<ani_size>(static_cast<unsigned int>(data[0]));
+    FuzzedDataProvider provider(reinterpret_cast<const uint8_t *>(data), size);
+    auto bufferSize = provider.ConsumeIntegral<ani_size>();
     env->CreateArrayBuffer(bufferSize, &dataResult, &arrayBufferRes);
 }
 }  // namespace OHOS
