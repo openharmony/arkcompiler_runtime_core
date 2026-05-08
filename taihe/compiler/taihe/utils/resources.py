860681c169b05d0bc87d5ce636caf3e4a66e303b
tongdiaoZS <1904807608@qq.com>
2026-05-08 14:37:12 +0800

    taihe: source migration to independent repository
    Taihe source migration to independent repository
    
    Issue: https://gitcode.com/openharmony/arkcompiler_runtime_core/issues/11262
    
    Co-Authored-By: NA
    
    Signed-off-by: tongdiaoZS <1904807608@qq.com>

--- taihe/compiler/taihe/utils/resources.py
+++ taihe/compiler/taihe/utils/resources.py
@@ -1,4 +1,4 @@
-# coding=utf-8
+# -*- coding: utf-8 -*-
 #
 # Copyright (c) 2025-2026 Huawei Device Co., Ltd.
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -384,8 +384,8 @@ class _LegacyPandaVm(PathResource):
 class PandaVm(CachedResource):
     CLI_NAME = "panda-vm"
     PATH_CACHE = "panda-vm"
-    VERSION: Final = "sdk-1.5.0-dev.57154"
-    URL: Final = "https://gitcode.com/m0_52007851/panda_vm/releases/download/57154"
+    VERSION: Final = "sdk-1.5.0-dev.63907"
+    URL: Final = "https://gitcode.com/m0_52007851/panda_vm/releases/download/63907"
 
     # Computed attributes
     ani_header_dir: Path = field(init=False)
@@ -461,6 +461,80 @@ class PandaVm(CachedResource):
         self._write_version(self.VERSION)
 
 
+class TaiheNapiBuild(CachedResource):
+    CLI_NAME = "napi-packages"
+    PATH_CACHE = "napi-packages"
+
+    # url for taihe napi packages
+    REPO: Final = (
+        "https://gitcode.com/m0_52007851/taihe-napi-bin/releases/download/v1.2.0/"
+    )
+
+    # Supported platforms
+    LINUX_X86_64: Final = "linux-x86_64"
+    WINDOWS_X86_64: Final = "windows-x86_64"
+    DARWIN_ARM64: Final = "darwin-arm64"
+    DARWIN_X86_64: Final = "darwin-x86_64"
+
+    # Bundle tarball file names for each platform
+    LINUX_X86_64_TAIHE_NAPI_BUNDLE: Final = (
+        "taihe-linux-x86_64-1.2.0+napi-20260112.tar.gz"
+    )
+    WINDOWS_X86_64_TAIHE_NAPI_BUNDLE: Final = (
+        "taihe-windows-x86_64-1.2.0+napi-20260112.tar.gz"
+    )
+    DARWIN_ARM64_TAIHE_NAPI_BUNDLE: Final = (
+        "taihe-darwin-arm64-1.2.0+napi-20260112.tar.gz"
+    )
+    DARWIN_X86_64_TAIHE_NAPI_BUNDLE: Final = (
+        "taihe-darwin-x86_64-1.2.0+napi-20260112.tar.gz"
+    )
+
+    files: ClassVar[dict[str, str]] = {
+        LINUX_X86_64: LINUX_X86_64_TAIHE_NAPI_BUNDLE,
+        WINDOWS_X86_64: WINDOWS_X86_64_TAIHE_NAPI_BUNDLE,
+        DARWIN_ARM64: DARWIN_ARM64_TAIHE_NAPI_BUNDLE,
+        DARWIN_X86_64: DARWIN_X86_64_TAIHE_NAPI_BUNDLE,
+    }
+
+    @override
+    def fetch(self):
+        # Clear and rebuild the cache directory
+        shutil.rmtree(self.base_path, ignore_errors=True)
+        self.base_path.mkdir(parents=True, exist_ok=True)
+
+        # System -> (repository url, file name)
+        downloads = {
+            self.LINUX_X86_64: (self.REPO, self.LINUX_X86_64_TAIHE_NAPI_BUNDLE),
+            self.WINDOWS_X86_64: (self.REPO, self.WINDOWS_X86_64_TAIHE_NAPI_BUNDLE),
+            self.DARWIN_ARM64: (self.REPO, self.DARWIN_ARM64_TAIHE_NAPI_BUNDLE),
+            self.DARWIN_X86_64: (self.REPO, self.DARWIN_X86_64_TAIHE_NAPI_BUNDLE),
+        }
+
+        # Download all platform bundles using curl
+        for _, (repo, filename) in downloads.items():
+            url = urljoin(repo, filename).replace("+", "%2B")
+            out_path = self.base_path / filename
+            logging.info("Downloading napi packages from %s to %s", url, out_path)
+            subprocess.run(["curl", "-fLsS", "-o", str(out_path), url], check=True)
+
+        logging.info("All napi packages downloaded to %s", self.base_path)
+
+    def extract_to(self, target_dir: Path, *, system: str):
+        tgz = self.base_path / self.files[system]
+
+        # Extract the tgz and get the directory list
+        with tarfile.open(tgz, "r:gz") as tar:
+            tar.extractall(target_dir, filter="tar")
+
+    @override
+    @classmethod
+    def construct(cls, ctx: ResourceContext) -> Self:
+        self = cls(cls.locate(ctx))
+        self.fetch()
+        return self
+
+
 class PythonBuild(CachedResource):
     CLI_NAME = "python-packages"
     PATH_CACHE = "python-packages"
@@ -486,6 +560,13 @@ class PythonBuild(CachedResource):
     WINDOWS_X86_64_PY_BUNDLE: Final = "python-mingw-x86-3.11.4_20251107.tar.gz"
     DARWIN_ARM64_PY_BUNDLE: Final = "python-darwin-arm64-3.11.4_20251107.tar.gz"
     DARWIN_X86_64_PY_BUNDLE: Final = "python-darwin-x86-3.11.4_20251107.tar.gz"
+
+    @override
+    @classmethod
+    def construct(cls, ctx: ResourceContext) -> Self:
+        self = cls(cls.locate(ctx))
+        self.fetch()
+        return self
 
     @override
     def fetch(self):
@@ -542,13 +623,6 @@ class PythonBuild(CachedResource):
             if dir_path != target_dir and dir_path.exists():
                 shutil.rmtree(dir_path, ignore_errors=True)
 
-    @override
-    @classmethod
-    def construct(cls, ctx: ResourceContext) -> Self:
-        self = cls(cls.locate(ctx))
-        self.fetch()
-        return self
-
 
 class Antlr(CachedResource):
     CLI_NAME = "antlr"
@@ -582,6 +656,7 @@ BUILTIN_RESOURCES: Sequence[ResourceT] = [
 ALL_RESOURCES: Sequence[ResourceT] = [
     *BUILTIN_RESOURCES,
     PandaVm,
+    TaiheNapiBuild,
     PythonBuild,
     Antlr,
 ]
--- taihe/scripts/common_lib.sh
+++ taihe/scripts/common_lib.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (c) 2025 Huawei Device Co., Ltd.
+# Copyright (c) 2025-2026 Huawei Device Co., Ltd.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -24,10 +24,142 @@ log_action() { echo -e "${COLOR_INV}${COLOR_BLUE}[-]${COLOR_RST} $*" >&2; }
 log_verbose() { echo -e "${COLOR_INV}${COLOR_GREY}[.]${COLOR_RST}${COLOR_GREY} $*${COLOR_RST}" >&2; }
 
 PROJECT_ROOT="$(realpath "$(dirname "$0")"/..)"
+TAIHE_FFI_GEN_VERSION="1.9.1"
+TAIHE_FFI_GEN_REPO_URLS=(
+    # https
+    "https://gitcode.com/openharmony-sig/arkcompiler_taihe_ffi_gen.git"
+    # SSH
+    # "git@gitcode.com:openharmony-sig/arkcompiler_taihe_ffi_gen.git"
+)
+TAIHE_FFI_GEN_RESTORED_PATHS=(
+    compiler
+    runtime
+    test
+    cmake
+)
+TAIHE_LOCAL_RESOURCES_REL_PATH="compiler/taihe/utils/resources.py"
 
 init_shell_options() { set -eu -o pipefail; }
 
 chdir_root() { cd "$PROJECT_ROOT"; }
+
+get_taihe_ffi_gen_branch() {
+    echo "${TAIHE_FFI_GEN_BRANCH:-v$TAIHE_FFI_GEN_VERSION}"
+}
+
+get_local_taihe_resources_source() {
+    if [[ -n "${TAIHE_LOCAL_RESOURCES_PATH:-}" ]]; then
+        echo "$TAIHE_LOCAL_RESOURCES_PATH"
+        return 0
+    fi
+    echo "$PROJECT_ROOT/$TAIHE_LOCAL_RESOURCES_REL_PATH"
+}
+
+backup_local_taihe_resources() {
+    local backup_root="$1"
+    local source_path
+    local project_source_path="$PROJECT_ROOT/$TAIHE_LOCAL_RESOURCES_REL_PATH"
+    source_path="$(get_local_taihe_resources_source)"
+
+    if [[ ! -f "$source_path" ]]; then
+        return 0
+    fi
+    if [[ -e "$source_path" && ! -f "$source_path" ]]; then
+        log_error "Local Taihe source path must be a file: $source_path"
+        return 1
+    fi
+
+    mkdir -p "$backup_root/$(dirname "$TAIHE_LOCAL_RESOURCES_REL_PATH")"
+    if [[ "$source_path" == "$project_source_path" ]]; then
+        log_stage "Temporarily move local $TAIHE_LOCAL_RESOURCES_REL_PATH"
+        mv "$source_path" "$backup_root/$TAIHE_LOCAL_RESOURCES_REL_PATH"
+    else
+        log_stage "Save local $TAIHE_LOCAL_RESOURCES_REL_PATH from $source_path"
+        cp -a "$source_path" "$backup_root/$TAIHE_LOCAL_RESOURCES_REL_PATH"
+    fi
+}
+
+restore_local_taihe_resources() {
+    local backup_root="$1"
+    local backup_path="$backup_root/$TAIHE_LOCAL_RESOURCES_REL_PATH"
+    local restore_path="$PROJECT_ROOT/$TAIHE_LOCAL_RESOURCES_REL_PATH"
+
+    if [[ ! -f "$backup_path" ]]; then
+        return 0
+    fi
+
+    mkdir -p "$PROJECT_ROOT/$(dirname "$TAIHE_LOCAL_RESOURCES_REL_PATH")"
+    cp -a "$backup_path" "$restore_path"
+    log_stage "Restore local $TAIHE_LOCAL_RESOURCES_REL_PATH"
+}
+
+restore_missing_taihe_sources() {
+    local has_missing_paths=0
+    local backup_root
+    local repo_urls=("${TAIHE_FFI_GEN_REPO_URLS[@]}")
+    local rel_path
+    for rel_path in "${TAIHE_FFI_GEN_RESTORED_PATHS[@]}"; do
+        if [[ ! -e "$PROJECT_ROOT/$rel_path" ]]; then
+            has_missing_paths=1
+            break
+        fi
+    done
+
+    if [[ $has_missing_paths -eq 0 ]]; then
+        return 0
+    fi
+
+    if ! command -v git &>/dev/null; then
+        log_error "git is required to restore missing Taihe sources"
+        return 1
+    fi
+
+    mkdir -p "$PROJECT_ROOT/.cache"
+
+    local branch clone_root repo_url
+    local cloned=0
+    branch="$(get_taihe_ffi_gen_branch)"
+    if [[ -n "${TAIHE_FFI_GEN_REPO:-}" ]]; then
+        repo_urls=("$TAIHE_FFI_GEN_REPO" "${repo_urls[@]}")
+    fi
+    clone_root="$(mktemp -d "$PROJECT_ROOT/.cache/arkcompiler_taihe_ffi_gen.XXXXXX")"
+    backup_root="$clone_root/local_backup"
+    backup_local_taihe_resources "$backup_root"
+
+    for repo_url in "${repo_urls[@]}"; do
+        rm -rf "$clone_root/repo"
+        log_stage "Fetch arkcompiler_taihe_ffi_gen branch $branch from $repo_url"
+        if git clone --depth 1 --branch "$branch" "$repo_url" "$clone_root/repo"; then
+            cloned=1
+            break
+        fi
+        log_error "Failed to fetch $repo_url branch $branch"
+    done
+
+    if [[ $cloned -ne 1 ]]; then
+        restore_local_taihe_resources "$backup_root"
+        rm -rf "$clone_root"
+        log_error "Unable to fetch arkcompiler_taihe_ffi_gen branch $branch"
+        return 1
+    fi
+
+    local src_path
+    for rel_path in "${TAIHE_FFI_GEN_RESTORED_PATHS[@]}"; do
+        src_path="$clone_root/repo/$rel_path"
+        if [[ ! -e "$src_path" ]]; then
+            restore_local_taihe_resources "$backup_root"
+            rm -rf "$clone_root"
+            log_error "arkcompiler_taihe_ffi_gen is missing $rel_path"
+            return 1
+        fi
+        log_stage "Restore $rel_path from arkcompiler_taihe_ffi_gen"
+        rm -rf "$PROJECT_ROOT/$rel_path"
+        cp -a "$src_path" "$PROJECT_ROOT/"
+    done
+
+    restore_local_taihe_resources "$backup_root"
+    rm -rf "$clone_root"
+}
 
 # Shorthand for running the standard uv.
 _uv() { uv --directory="$PROJECT_ROOT" "$@"; }
