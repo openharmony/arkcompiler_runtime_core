2d22144f9baa21b71b729834b22356f1d21bec59
tianlikai_ <tianlikai@h-partners.com>
2026-06-26 16:55:32 +0800

    fix interop overload
    Issue: https://gitcode.com/openharmony/arkcompiler_runtime_core/issues/11909
    
    Co-Authored-By: Agent
    
    Signed-off-by: tianlikai <tianlikai@h-partners.com>
    Change-Id: Icf2bc5a4fa75b836aca5753d9f4fd89e7c2dbd05

--- static_core/plugins/ets/tests/interop_js/tests/overload/ets_to_ts/test_overload.ts
+++ static_core/plugins/ets/tests/interop_js/tests/overload/ets_to_ts/test_overload.ts
@@ -105,6 +105,339 @@ function testGenericOverload(): void {
     ASSERT_TRUE(genericOverloadRule().mergeRule('hello') === 'generic');
 }
 
+function testObjectOverloadMutuallyExclusive(): void {
+    let createHandler = etsVm.getFunction('Ltest_overload/ETSGLOBAL;', 'createObjectOverloadHandler');
+    let createDog = etsVm.getFunction('Ltest_overload/ETSGLOBAL;', 'createDog');
+    let createCat = etsVm.getFunction('Ltest_overload/ETSGLOBAL;', 'createCat');
+
+    let handler = createHandler();
+    let dog = createDog();
+    let cat = createCat();
+
+    ASSERT_TRUE(handler.handlePet(dog) === 'dog', 'Expected "dog" but got: ' + handler.handlePet(dog));
+    ASSERT_TRUE(handler.handlePet(cat) === 'cat', 'Expected "cat" but got: ' + handler.handlePet(cat));
+}
+
+function testObjectOverloadSpecificVsObject(): void {
+    let createHandler = etsVm.getFunction('Ltest_overload/ETSGLOBAL;', 'createObjectOverloadHandler');
+    let createDog = etsVm.getFunction('Ltest_overload/ETSGLOBAL;', 'createDog');
+
+    let handler = createHandler();
+    let dog = createDog();
+
+    ASSERT_TRUE(handler.handleSpecific(dog) === 'dog',
+        'Expected "dog" (specific overload) but got: ' + handler.handleSpecific(dog));
+
+    try {
+        handler.handleSpecific({});
+        ASSERT_TRUE(false, 'Expected TypeError for plain JS object');
+    } catch (e) {
+        ASSERT_TRUE(e.message.includes('not assignable'),
+            'Expected "not assignable" error for {} but got: ' + e.message);
+    }
+
+    try {
+        handler.handleSpecific(null);
+        ASSERT_TRUE(false, 'Expected TypeError for null');
+    } catch (e) {
+        ASSERT_TRUE(e.message.includes('not assignable'),
+            'Expected "not assignable" error for null but got: ' + e.message);
+    }
+
+    ASSERT_TRUE(handler.handleSpecific(undefined) === 'dog',
+        'Expected "dog" for undefined (dispatched to Dog overload) but got: ' + handler.handleSpecific(undefined));
+}
+
+function testObjectOverloadInheritance(): void {
+    let createHandler = etsVm.getFunction('Ltest_overload/ETSGLOBAL;', 'createObjectOverloadHandler');
+    let createDog = etsVm.getFunction('Ltest_overload/ETSGLOBAL;', 'createDog');
+    let createAnimal = etsVm.getFunction('Ltest_overload/ETSGLOBAL;', 'createAnimal');
+
+    let handler = createHandler();
+    let dog = createDog();
+    let animal = createAnimal();
+
+    ASSERT_TRUE(handler.handleAnimal(dog) === 'dog',
+        'Expected "dog" for Dog instance but got: ' + handler.handleAnimal(dog));
+    ASSERT_TRUE(handler.handleAnimal(animal) === 'animal',
+        'Expected "animal" for Animal instance but got: ' + handler.handleAnimal(animal));
+}
+
+function testIntVsAny(): void {
+    let createHandler = etsVm.getFunction('Ltest_overload/ETSGLOBAL;', 'createObjectOverloadPrimitiveHandler');
+    let handler = createHandler();
+
+    ASSERT_TRUE(handler.handleIntOrAny(42) === 'int',
+        'Expected "int" for integer but got: ' + handler.handleIntOrAny(42));
+
+    ASSERT_TRUE(handler.handleIntOrAny({a: 1}) === 'any',
+        'Expected "any" for JS object but got: ' + handler.handleIntOrAny({a: 1}));
+
+    ASSERT_TRUE(handler.handleIntOrAny(null) === 'any',
+        'Expected "any" for null but got: ' + handler.handleIntOrAny(null));
+
+    ASSERT_TRUE(handler.handleIntOrAny(undefined) === 'any',
+        'Expected "any" for undefined but got: ' + handler.handleIntOrAny(undefined));
+
+    ASSERT_TRUE(handler.handleIntOrAny(true) === 'any',
+        'Expected "any" for boolean but got: ' + handler.handleIntOrAny(true));
+
+    ASSERT_TRUE(handler.handleIntOrAny('hello') === 'any',
+        'Expected "any" for string but got: ' + handler.handleIntOrAny('hello'));
+}
+
+function testStringVsAny(): void {
+    let createHandler = etsVm.getFunction('Ltest_overload/ETSGLOBAL;', 'createObjectOverloadPrimitiveHandler');
+    let handler = createHandler();
+
+    ASSERT_TRUE(handler.handleStringOrAny('hello') === 'string',
+        'Expected "string" for string but got: ' + handler.handleStringOrAny('hello'));
+
+    ASSERT_TRUE(handler.handleStringOrAny(42) === 'any',
+        'Expected "any" for number but got: ' + handler.handleStringOrAny(42));
+
+    ASSERT_TRUE(handler.handleStringOrAny(null) === 'any',
+        'Expected "any" for null but got: ' + handler.handleStringOrAny(null));
+
+    ASSERT_TRUE(handler.handleStringOrAny(undefined) === 'any',
+        'Expected "any" for undefined but got: ' + handler.handleStringOrAny(undefined));
+
+    ASSERT_TRUE(handler.handleStringOrAny(true) === 'any',
+        'Expected "any" for boolean but got: ' + handler.handleStringOrAny(true));
+}
+
+function testAnyVsInt(): void {
+    let createHandler = etsVm.getFunction('Ltest_overload/ETSGLOBAL;', 'createObjectOverloadPrimitiveHandler');
+    let handler = createHandler();
+
+    ASSERT_TRUE(handler.handleAnyOrInt({a: 1}) === 'any',
+        'Expected "any" for JS object but got: ' + handler.handleAnyOrInt({a: 1}));
+
+    ASSERT_TRUE(handler.handleAnyOrInt(null) === 'any',
+        'Expected "any" for null but got: ' + handler.handleAnyOrInt(null));
+
+    ASSERT_TRUE(handler.handleAnyOrInt(undefined) === 'any',
+        'Expected "any" for undefined but got: ' + handler.handleAnyOrInt(undefined));
+}
+
+function testDogVsAny(): void {
+    let createHandler = etsVm.getFunction('Ltest_overload/ETSGLOBAL;', 'createObjectOverloadPrimitiveHandler');
+    let createDog = etsVm.getFunction('Ltest_overload/ETSGLOBAL;', 'createDog');
+    let handler = createHandler();
+    let dog = createDog();
+
+    ASSERT_TRUE(handler.handleDogOrAny(dog) === 'dog',
+        'Expected "dog" for Dog instance but got: ' + handler.handleDogOrAny(dog));
+
+    try {
+        handler.handleDogOrAny({a: 1});
+        ASSERT_TRUE(false, 'Expected TypeError for plain JS object');
+    } catch (e) {
+        ASSERT_TRUE(e.message.includes('not assignable'),
+            'Expected "not assignable" error for {} but got: ' + e.message);
+    }
+
+    try {
+        handler.handleDogOrAny(null);
+        ASSERT_TRUE(false, 'Expected TypeError for null');
+    } catch (e) {
+        ASSERT_TRUE(e.message.includes('not assignable'),
+            'Expected "not assignable" error for null but got: ' + e.message);
+    }
+}
+
+function testCheckStringVsAny(): void {
+    let createHandler = etsVm.getFunction('Ltest_overload/ETSGLOBAL;', 'createCheckArgMatchObjectHandler');
+    let handler = createHandler();
+
+    ASSERT_TRUE(handler.handleStringOrAny('hello') === 'string',
+        'Expected "string" for string but got: ' + handler.handleStringOrAny('hello'));
+
+    ASSERT_TRUE(handler.handleStringOrAny(42) === 'any',
+        'Expected "any" for number but got: ' + handler.handleStringOrAny(42));
+
+    ASSERT_TRUE(handler.handleStringOrAny(null) === 'any',
+        'Expected "any" for null but got: ' + handler.handleStringOrAny(null));
+
+    ASSERT_TRUE(handler.handleStringOrAny(undefined) === 'any',
+        'Expected "any" for undefined but got: ' + handler.handleStringOrAny(undefined));
+}
+
+function testCheckBigIntVsAny(): void {
+    let createHandler = etsVm.getFunction('Ltest_overload/ETSGLOBAL;', 'createCheckArgMatchObjectHandler');
+    let handler = createHandler();
+
+    ASSERT_TRUE(handler.handleBigIntOrAny(123n) === 'bigint',
+        'Expected "bigint" for bigint but got: ' + handler.handleBigIntOrAny(123n));
+
+    ASSERT_TRUE(handler.handleBigIntOrAny(42) === 'any',
+        'Expected "any" for number but got: ' + handler.handleBigIntOrAny(42));
+
+    ASSERT_TRUE(handler.handleBigIntOrAny(null) === 'any',
+        'Expected "any" for null but got: ' + handler.handleBigIntOrAny(null));
+}
+
+function testFunctionVsInt(): void {
+    let createHandler = etsVm.getFunction('Ltest_overload/ETSGLOBAL;', 'createCheckArgMatchObjectHandler');
+    let handler = createHandler();
+
+    ASSERT_TRUE(handler.handleFnOrAny(42) === 'int',
+        'Expected "int" for number but got: ' + handler.handleFnOrAny(42));
+
+    ASSERT_TRUE(handler.handleFnOrAny(function() {}) === 'function',
+        'Expected "function" for function but got: ' + handler.handleFnOrAny(function() {}));
+
+    ASSERT_TRUE(handler.handleFnOrAny({a: 1}) === 'function',
+        'Expected "function" for JS object but got: ' + handler.handleFnOrAny({a: 1}));
+
+    ASSERT_TRUE(handler.handleFnOrAny(null) === 'function',
+        'Expected "function" for null but got: ' + handler.handleFnOrAny(null));
+}
+
+function testBoolVsAny(): void {
+    let createHandler = etsVm.getFunction('Ltest_overload/ETSGLOBAL;', 'createCheckArgMatchObjectHandler');
+    let handler = createHandler();
+
+    ASSERT_TRUE(handler.handleBoolOrAny(true) === 'boolean',
+        'Expected "boolean" for boolean but got: ' + handler.handleBoolOrAny(true));
+
+    ASSERT_TRUE(handler.handleBoolOrAny(false) === 'boolean',
+        'Expected "boolean" for false but got: ' + handler.handleBoolOrAny(false));
+
+    ASSERT_TRUE(handler.handleBoolOrAny(42) === 'any',
+        'Expected "any" for number but got: ' + handler.handleBoolOrAny(42));
+
+    ASSERT_TRUE(handler.handleBoolOrAny(null) === 'any',
+        'Expected "any" for null but got: ' + handler.handleBoolOrAny(null));
+
+    ASSERT_TRUE(handler.handleBoolOrAny({a: 1}) === 'any',
+        'Expected "any" for JS object but got: ' + handler.handleBoolOrAny({a: 1}));
+}
+
+function testDoubleVsAny(): void {
+    let createHandler = etsVm.getFunction('Ltest_overload/ETSGLOBAL;', 'createCheckArgMatchObjectHandler');
+    let handler = createHandler();
+
+    ASSERT_TRUE(handler.handleDoubleOrAny(1.5) === 'double',
+        'Expected "double" for double but got: ' + handler.handleDoubleOrAny(1.5));
+
+    ASSERT_TRUE(handler.handleDoubleOrAny(1e40) === 'double',
+        'Expected "double" for large double but got: ' + handler.handleDoubleOrAny(1e40));
+
+    ASSERT_TRUE(handler.handleDoubleOrAny(42) === 'double',
+        'Expected "double" for integer number but got: ' + handler.handleDoubleOrAny(42));
+
+    ASSERT_TRUE(handler.handleDoubleOrAny(null) === 'any',
+        'Expected "any" for null but got: ' + handler.handleDoubleOrAny(null));
+
+    ASSERT_TRUE(handler.handleDoubleOrAny({a: 1}) === 'any',
+        'Expected "any" for JS object but got: ' + handler.handleDoubleOrAny({a: 1}));
+}
+
+function testMultiArgOverload(): void {
+    let createHandler = etsVm.getFunction('Ltest_overload/ETSGLOBAL;', 'createCheckArgMatchObjectHandler');
+    let handler = createHandler();
+
+    ASSERT_TRUE(handler.handleTwoArgs('hello', 42) === 'string int',
+        'Expected "string int" for (string, number) but got: ' + handler.handleTwoArgs('hello', 42));
+
+    ASSERT_TRUE(handler.handleTwoArgs(42, 42) === 'any any',
+        'Expected "any any" for (number, number) but got: ' + handler.handleTwoArgs(42, 42));
+
+    ASSERT_TRUE(handler.handleTwoArgs({}, null) === 'any any',
+        'Expected "any any" for (object, null) but got: ' + handler.handleTwoArgs({}, null));
+
+    ASSERT_TRUE(handler.handleTwoArgs('hello', {}) === 'any any',
+        'Expected "any any" for (string, object) but got: ' + handler.handleTwoArgs('hello', {}));
+}
+
+function testInheritanceChainWithAny(): void {
+    let createHandler = etsVm.getFunction('Ltest_overload/ETSGLOBAL;', 'createCheckArgMatchObjectHandler');
+    let createDog = etsVm.getFunction('Ltest_overload/ETSGLOBAL;', 'createDog');
+    let createAnimal = etsVm.getFunction('Ltest_overload/ETSGLOBAL;', 'createAnimal');
+    let handler = createHandler();
+    let dog = createDog();
+    let animal = createAnimal();
+
+    ASSERT_TRUE(handler.handleDogOrAnimalOrAny(dog) === 'dog',
+        'Expected "dog" for Dog instance but got: ' + handler.handleDogOrAnimalOrAny(dog));
+
+    ASSERT_TRUE(handler.handleDogOrAnimalOrAny(animal) === 'animal',
+        'Expected "animal" for Animal instance but got: ' + handler.handleDogOrAnimalOrAny(animal));
+
+    try {
+        handler.handleDogOrAnimalOrAny({});
+        ASSERT_TRUE(false, 'Expected TypeError for plain JS object');
+    } catch (e) {
+        ASSERT_TRUE(e.message.includes('not assignable'),
+            'Expected "not assignable" error for {} but got: ' + e.message);
+    }
+
+    try {
+        handler.handleDogOrAnimalOrAny(null);
+        ASSERT_TRUE(false, 'Expected TypeError for null');
+    } catch (e) {
+        ASSERT_TRUE(e.message.includes('not assignable'),
+            'Expected "not assignable" error for null but got: ' + e.message);
+    }
+
+    ASSERT_TRUE(handler.handleDogOrAnimalOrAny(undefined) === 'dog',
+        'Expected "dog" for undefined (dispatched to Dog overload) but got: ' + handler.handleDogOrAnimalOrAny(undefined));
+}
+
+function testThreeArgSpecificVsAny(): void {
+    let createHandler = etsVm.getFunction('Ltest_overload/ETSGLOBAL;', 'createCheckArgMatchObjectHandler');
+    let createDog = etsVm.getFunction('Ltest_overload/ETSGLOBAL;', 'createDog');
+    let handler = createHandler();
+    let dog = createDog();
+
+    ASSERT_TRUE(handler.handleThreeSpecific('hello', 42, dog) === 'string int dog',
+        'Expected "string int dog" for (string, int, Dog) but got: ' + handler.handleThreeSpecific('hello', 42, dog));
+
+    ASSERT_TRUE(handler.handleThreeSpecific(42, 42, dog) === 'any any any',
+        'Expected "any any any" for (number, int, Dog) but got: ' + handler.handleThreeSpecific(42, 42, dog));
+
+    try {
+        handler.handleThreeSpecific('hello', 42, {});
+        ASSERT_TRUE(false, 'Expected TypeError for {} as Dog arg');
+    } catch (e) {
+        ASSERT_TRUE(e.message.includes('not assignable'),
+            'Expected "not assignable" error for {} as Dog but got: ' + e.message);
+    }
+
+    ASSERT_TRUE(handler.handleThreeSpecific('hello', {}, dog) === 'any any any',
+        'Expected "any any any" for (string, object, Dog) but got: ' + handler.handleThreeSpecific('hello', {}, dog));
+
+    ASSERT_TRUE(handler.handleThreeSpecific({}, null, undefined) === 'any any any',
+        'Expected "any any any" for (object, null, undefined) but got: ' + handler.handleThreeSpecific({}, null, undefined));
+}
+
+function testThreeArgInheritanceChain(): void {
+    let createHandler = etsVm.getFunction('Ltest_overload/ETSGLOBAL;', 'createCheckArgMatchObjectHandler');
+    let createDog = etsVm.getFunction('Ltest_overload/ETSGLOBAL;', 'createDog');
+    let createAnimal = etsVm.getFunction('Ltest_overload/ETSGLOBAL;', 'createAnimal');
+    let handler = createHandler();
+    let dog = createDog();
+    let animal = createAnimal();
+
+    ASSERT_TRUE(handler.handleThreeInherit(dog, dog, dog) === 'dog dog dog',
+        'Expected "dog dog dog" for (Dog, Dog, Dog) but got: ' + handler.handleThreeInherit(dog, dog, dog));
+
+    ASSERT_TRUE(handler.handleThreeInherit(animal, animal, animal) === 'animal animal any',
+        'Expected "animal animal any" for (Animal, Animal, Animal) but got: ' + handler.handleThreeInherit(animal, animal, animal));
+
+    ASSERT_TRUE(handler.handleThreeInherit(animal, animal, null) === 'animal animal any',
+        'Expected "animal animal any" for (Animal, Animal, null) but got: ' + handler.handleThreeInherit(animal, animal, null));
+
+    try {
+        handler.handleThreeInherit({}, {}, {});
+        ASSERT_TRUE(false, 'Expected TypeError for {} args');
+    } catch (e) {
+        ASSERT_TRUE(e.message.includes('not assignable'),
+            'Expected "not assignable" error for {} args but got: ' + e.message);
+    }
+}
+
 function testAll(): void {
     testExtendsOverloadConflict();
     testStaInsOverload();
@@ -112,6 +445,22 @@ function testAll(): void {
     testTypeOverload();
     testComplexOverload();
     testGenericOverload();
+    testObjectOverloadMutuallyExclusive();
+    testObjectOverloadSpecificVsObject();
+    testObjectOverloadInheritance();
+    testIntVsAny();
+    testStringVsAny();
+    testAnyVsInt();
+    testDogVsAny();
+    testCheckStringVsAny();
+    testCheckBigIntVsAny();
+    testFunctionVsInt();
+    testBoolVsAny();
+    testDoubleVsAny();
+    testMultiArgOverload();
+    testInheritanceChainWithAny();
+    testThreeArgSpecificVsAny();
+    testThreeArgInheritanceChain();
 }
 
 function main(): void {
