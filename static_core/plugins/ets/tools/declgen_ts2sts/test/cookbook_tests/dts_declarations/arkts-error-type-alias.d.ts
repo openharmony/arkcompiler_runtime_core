c6edd4f586e61583415a968e9fb1d13eee9dd21f
wanquan <wanquan8@h-partners.com>
2026-06-27 14:33:01 +0800

    fix_APIError_mismatch
    Issue:https://gitcode.com/openharmony/arkcompiler_runtime_core/issues/11953
    
    Signed-off-by: wanquan <wanquan8@h-partners.com>
    
    Co-Authored-By: Agent

---
+++ static_core/plugins/ets/tools/declgen_ts2sts/test/cookbook_tests/dts_declarations/arkts-error-type-alias.d.ts
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2026 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+export class APIError extends Error{
+  message: string;
+}
+export class APIError1 implements Error{
+  name: string;
+  stack?: string | undefined
+  cause?: unknown
+  message: string;
+}
+export class APIError2 extends APIError1{}
+export class APIError3 extends APIError2{}
+export class APIError4 extends APIError{
+  message: string;
+}
+export class APIError5 implements APIError5{}
+export class APIError6 extends APIError1 implements APIError6{}
